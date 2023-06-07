e55fbe80b3cbb479aebd31dfeef3abacbbd130d7
VioletAi <54014799+VioletAi@users.noreply.github.com>
2023-06-07 17:34:24 +1000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./styles.css";
+
+export default function App() {
+  const todos = [
+    { id: 3, text: "Wash dishes", done: false },
+    { id: 2, text: "Do laundry", done: false },
+    { id: 1, text: "Take shower", done: false }
+  ];
+
+  return (
+    <div className="App">
+      <h1>TodoList</h1>
+
+      <TodoList what={todos} />
+    </div>
+  );
+}
+
+function TodoList({ what }) {
+  return (
+    <ul>
+      {what.map((todo) => (
+        <li key={todo.id}>{todo.text}</li>
+      ))}
+    </ul>
+  );
+}
